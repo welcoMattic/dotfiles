bfc54432f991fcca3eb2c7d611314c209eb52f97
Mathieu Santostefano <msantostefano@protonmail.com>
2022-08-10 15:32:14 +0200

    Add dot_config/scripts/executable_notify_volume_and_brightness.sh

---
+++ dot_config/scripts/executable_notify_volume_and_brightness.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+notify_volume() {
+	if [ "$(pamixer --get-mute)" = "true" ]; then
+		notify-send -a progress -t 1000 -h 'string:wired-tag:volume' -h "int:value:$(pamixer --get-volume)" 'Volume' 'muted'
+	else
+		notify-send -a progress -t 1000 -h 'string:wired-tag:volume' -h "int:value:$(pamixer --get-volume)" 'Volume' 
+	fi
+}
+
+notify_brightness() {
+	notify-send -a progress -t 1000 -h 'string:wired-tag:brightness' -h "int:value:$target" 'Brightness' 
+}
+
+case "$1" in
+	up)
+		pamixer -i 5
+		notify_volume
+	;;
+
+	down)
+		pamixer -d 5
+		notify_volume
+	;;
+
+	mutetoggle)
+		pamixer --toggle-mute
+		notify_volume
+	;;
+
+	light_up)
+                xbacklight -inc 10
+                target=$(printf '%.*f\n' 0 "$(xbacklight)")
+		notify_brightness
+	;;
+
+	light_down)
+                xbacklight -dec 10
+                target=$(printf '%.*f\n' 0 "$(xbacklight)")
+		notify_brightness
+	;;
+esac
